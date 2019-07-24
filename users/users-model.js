5e95e99d99ee081bb1296cd803018913fbe1e6c6
LadyKerr <kadiekerr@gmail.com>
2019-07-24 14:23:03 -0400

    lecture updates

--- users/users-model.js
+++ users/users-model.js
@@ -1,28 +1,34 @@
-const db = require('../database/dbConfig.js');
+const db = require("../database/dbConfig.js");
 
 module.exports = {
   add,
   find,
   findBy,
-  findById,
+  findById
 };
 
-function find() {
-  return db('users').select('id', 'username', 'password');
+function find(department) {
+  const query = db("users").select("id", "username", "department");
+
+  if (department) {
+    query.where({ department });
+  }
+
+  return query;
 }
 
 function findBy(filter) {
-  return db('users').where(filter);
+  return db("users").where(filter);
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user);
+  const [id] = await db("users").insert(user);
 
   return findById(id);
 }
 
 function findById(id) {
-  return db('users')
+  return db("users")
     .where({ id })
     .first();
 }
--- users/users-router.js
+++ users/users-router.js
@@ -1,10 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('./users-model.js');
-const restricted = require('../auth/restricted-middleware.js');
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.get('/', restricted, (req, res) => {
-  Users.find()
+router.get("/", restricted, (req, res) => {
+  // console.log("req.jwtToken", req.jwtToken);
+
+  const department = req.jwtToken.department;
+// users can only see other users in their departments.
+  Users.find(department)
     .then(users => {
       res.json(users);
     })
